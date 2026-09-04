5120cc3cd45460d181a53eb8a4699666dc841aec
Jackson Giemza <jackson.giemza@gmail.com>
2026-09-04 18:24:08 -0500

    more refactoring and adding multi product processing

---
+++ src/crypto_arb/book_processing.py
@@ -0,0 +1,81 @@
+import heapq
+from crypto_arb.tools import timer
+
+@timer
+def apply_updates(data, books):
+    if data.get('channel') != 'l2_data':
+        return None, None
+
+    events = data["events"]
+
+    updated_products = []
+    update_count = 0
+    for event in events:
+
+        product = event["product_id"]
+        product_book = books[product]
+        updated_products.append(product)
+
+        for update in event["updates"]:
+            update_count += 1
+
+            side = update["side"]
+            price = float(update["price_level"])
+            quantity = update["new_quantity"]
+
+            if side == "bid":
+                book = product_book["bids"]
+                heap = product_book["bid_heap"]
+                order = -price
+            else:
+                book = product_book["asks"]
+                heap = product_book["ask_heap"]
+                order = price
+            
+            if quantity == "0":
+                book.pop(price, None)
+
+            else:
+                if price not in book:
+                    heapq.heappush(heap, order)
+
+                book[price] = quantity
+
+    return update_count, updated_products
+
+@timer
+def rebalance_book(books, updated_products):
+    if not updated_products:
+        return 
+    
+    for product in updated_products:
+
+        book = books[product]
+        bids = book["bids"]
+        asks = book["asks"]
+        bid_heap = book["bid_heap"]
+        ask_heap = book["ask_heap"]
+            
+        # remove stale bids
+        while bid_heap and -bid_heap[0] not in bids:
+            heapq.heappop(bid_heap)
+
+        # remove stale asks
+        while ask_heap and ask_heap[0] not in asks:
+            heapq.heappop(ask_heap)
+
+def print_top_of_book(books, updated_products):
+    if not updated_products:
+        return
+    
+    for product in updated_products:
+        book = books[product]
+        if len(book["bid_heap"]) == 0 or len(book["ask_heap"]) == 0:
+            continue
+
+        bids = book['bids']
+        asks = book['asks']
+        best_bid_price = -book["bid_heap"][0]
+        best_ask_price = book["ask_heap"][0]
+        
+        print(f"{product} | Bid: ${best_bid_price}, {bids[best_bid_price]} QTY | Ask: ${best_ask_price}, {asks[best_ask_price]} QTY")
--- src/crypto_arb/coinbase.py
+++ src/crypto_arb/coinbase.py
@@ -1,18 +1,21 @@
 from websockets.asyncio.client import connect
 from websockets.exceptions import ConnectionClosedError, ConnectionClosedOK
-from crypto_arb.processing import apply_updates, log_book, print_book
+from crypto_arb.book_processing import apply_updates, rebalance_book, print_top_of_book
 from crypto_arb.tools import add_update_count, report_stats
 import asyncio
 import json
 
 class CoinbaseAdapter:
-    def __init__(self, URL, products, subscription):
-        self.URL = URL,
-        self.subscription = json.dumps(subscription)
-        self.products = products,
-
+    def __init__(self, products):
+        self.URL = "wss://advanced-trade-ws.coinbase.com"
+        self.subscription = {
+            "type": "subscribe",
+            "product_ids": products,
+            "channel": "level2"
+        }
+        self.products = products
         self.queue = asyncio.Queue(maxsize=1000)
-        self.books = {}
+        self.books = self._init_books()
         self.RESET = object()
 
     async def receiver(self):
@@ -20,7 +23,7 @@ class CoinbaseAdapter:
             try:
                 async with connect(self.URL, max_size=None) as ws:
                     await self.queue.put(self.RESET)
-                    await ws.send(self.subscription)
+                    await ws.send(json.dumps(self.subscription))
 
                     async for message in ws:
                         await self.queue.put(message)
@@ -30,40 +33,42 @@ class CoinbaseAdapter:
                 await asyncio.sleep(1)
 
     async def processor(self):
-        bids = {}
-        bid_heap = []
-
-        asks = {}
-        ask_heap = [] 
-
         while True:
             message = await self.queue.get()
             
             if message is self.RESET:
-                bids.clear()
-                asks.clear()
-                bid_heap.clear()
-                ask_heap.clear()
+                self.books = self._init_books()
                 continue
 
             data = json.loads(message)
 
-            update_count = apply_updates(
-                data, bids, asks, bid_heap, ask_heap
-            )
+            update_count, updated_products = apply_updates(data, self.books)
             add_update_count("apply_updates", update_count)
-
-            best_bid_price, best_ask_price = log_book(
-                bids, asks, bid_heap, ask_heap
-            )
-
-            if best_bid_price is not None and best_ask_price is not None:
-                print_book(
-                    best_bid_price, best_ask_price, bids, asks
-                )
+            rebalance_book(self.books, updated_products)
+            print_top_of_book(self.books, updated_products)
 
     async def run(self):
         async with asyncio.TaskGroup() as tg:
             tg.create_task(self.receiver())
             tg.create_task(self.processor())
+
+    def _init_books(self):
+        books = {}
+        for product in self.products:
+            books[product] = {
+                "bids": {},
+                "bid_heap": [],
+                "asks": {},
+                "ask_heap": [],
+            }
+        return books
+
+if __name__ == "__main__":
+    coinbase = CoinbaseAdapter(products=["BTC-USD", "ETH-USD"])
+
+    try:
+        asyncio.run(coinbase.run())
         
+    except KeyboardInterrupt:
+        print("Exiting WebSocket..")
+        report_stats()
